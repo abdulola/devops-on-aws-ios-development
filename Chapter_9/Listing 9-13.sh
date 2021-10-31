fd80a300859370e09a5bb800317dda206039f225
Abdullahi Olaoye <abdullahiolaoye4@gmail.com>
2021-10-31 08:16:52 -0500

    Update chatpter 9 code

--- Chapter_9/Listing 9-13.sh
+++ Chapter_9/Listing 9-13.sh
@@ -1,6 +1,6 @@
 set +x
 
-export TEAM_ID=`aws secretsmanager get-secret-value --secret-id fastlane-secrets --query SecretString --profile devops-ios --output text | jq -r .TEAM_ID`
+export TEAM_ID=`aws secretsmanager get-secret-value --secret-id fastlane-secrets --query SecretString --output text | jq -r .TEAM_ID`
 
 /usr/bin/xcodebuild build-for-testing -scheme SampleApp -destination generic/platform=iOS DEVELOPMENT_TEAM=$TEAM_ID -allowProvisioningUpdates -derivedDataPath $WORKSPACE
 mkdir Payload && cp -r $WORKSPACE/Build/Products/Debug-iphoneos/SampleApp.app Payload/
